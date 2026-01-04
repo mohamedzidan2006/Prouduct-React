b0414ead99220c78daccffe972093a983caba18d
mohamedzidan2006 <a.z01150171478@gmail.com>
2026-01-04 21:33:50 +0200

    link-Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Shop from './Shop';
 import Product from './Product';
 export default function App() {
   return ( 
     <div className='w-full  '>
     <BrowserRouter>
+     <Link className="btn btn-secondary" to={'/Shop'}>ShopPage </Link>
     <Routes>
       <Route path="/Shop" element={<Shop/>}></Route>
      <Route path="/Shop/Product/:ProductId" element={<Product/>} ></Route> 
